161cdd4b25f6d9a06dd628b06b9acb4da62da1b1
ena-isme <dev.shinena@gmail.com>
2025-05-15 14:00:26 +0900

    create project

---
+++ putrack/putrack/putrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  putrackApp.swift
+//  putrack
+//
+//  Created by 신지원 on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct putrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
